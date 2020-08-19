866bf54c869c6e9898150ec1bb668f3ab8d3b2be
Ian Thomas <ian.thomas@skybettingandgaming.com>
2020-08-19 22:32:55 +0100

    Adds images and removes cloudflare

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
 module.exports = config => {
+
+  config.addPassthroughCopy("src/site/img");
+  config.addPassthroughCopy("src/site/js");
+  config.addPassthroughCopy("src/site/css");
+
   return {
     dir: {
       input: "src/site",
-      includes: "src/site/__includes"
+      includes: "src/site/__includes",
+      output: "tmp"
     }
   }
 }
